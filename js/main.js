2d0538f2783747f812fbb39a032350eb376b08e0
Hi-rien <098sung@hanmail.net>
2023-07-19 17:32:29 +0900

    mobile 반응형 1차

--- js/main.js
+++ js/main.js
@@ -172,4 +172,52 @@ function blockOpen() {
 }
 
 
+//---------------------------- Mobile
 
+
+// 모바일 skills
+
+let oldId = null;
+
+$('.tabs-controls__link').click(function(e) {
+  e.preventDefault();
+
+  if ($(this).hasClass('tabs-controls__link--active')) {
+    return false;
+  }
+
+  let currentId = parseInt($(this).data('id'), 10);
+  $('.tabs-controls__link--active').removeClass('tabs-controls__link--active');
+  $(this).addClass('tabs-controls__link--active');
+
+  if (currentId < oldId) { // item is hidden
+    let timing = $('.card.hidden').length * 100;
+    $('.card').each(function(index) {
+      if (index > (currentId - 1 ) || index == (currentId - 1)) {
+        window.setTimeout(function() {
+          $('.card').eq(index).removeClass('hidden');
+        }, timing - (index * 100));
+      }
+    });
+  } else {
+    $('.card').each(function(index) {
+      if (index < (currentId - 1)) {
+        window.setTimeout(function() {
+          $('.card').eq(index).addClass('hidden');
+        }, index * 100);
+      }
+    });
+  }
+
+  oldId = currentId;
+
+  let aniIndex = $(this).data('id')
+  console.log(aniIndex)
+  $(`.cards-container #${aniIndex}`).addClass('start').siblings().removeClass('start')
+});
+
+$('#msection2').waypoint(function() {
+  $('#1').addClass('start')
+}, {
+  offset: '75%'
+})
